e65883f6ee365dc848584898dc4d133feb160f58
atorresme <artorresm@hotmail.com>
2019-10-23 16:09:18 +0200

    eliminamos

--- workspace/LP_PRUEBA1/poms/jobs/process/etl_prueba_2_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/dom4j-1.6.1.jar;../lib/jtds-1.3.1-patch.jar;../lib/log4j-1.2.17.jar;../lib/talend_DB_mssqlUtil-1.2-20171017.jar;../lib/talendcsv.jar;etl_prueba_2_0_1.jar;' lp_prueba1.etl_prueba_2_0_1.ETL_Prueba_2  %*
--- workspace/LP_PRUEBA1/poms/jobs/process/job_prueba_1_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/commons-collections-3.2.2.jar;../lib/dom4j-1.6.1.jar;../lib/jboss-serialization.jar;../lib/jtds-1.3.1-patch.jar;../lib/log4j-1.2.17.jar;../lib/talend_DB_mssqlUtil-1.2-20171017.jar;../lib/talendcsv.jar;../lib/trove.jar;job_prueba_1_0_1.jar;' lp_prueba1.job_prueba_1_0_1.Job_Prueba_1  %*
